f48e8659672e1fb88ad2f2e71a31eeec78513eb2
Guanyi Yang <50741392+Qiguanyi@users.noreply.github.com>
2020-07-02 14:02:05 -0400

    Add files via upload

--- Part II - Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
+++ Part II - Data Structures/week2_priority_queues_and_disjoint_sets/2_job_queue/job_queue.py
@@ -1,31 +1,78 @@
 # python3
 
-from collections import namedtuple
+import heapq
+#from collections import namedtuple
+#
+#AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
+#
+#
+#def assign_jobs(n_workers, jobs):
+#    # TODO: replace this code with a faster algorithm.
+#    result = []
+#    next_free_time = [0] * n_workers
+#    for job in jobs:
+#        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
+#        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
+#        next_free_time[next_worker] += job
+#
+#    return result
 
-AssignedJob = namedtuple("AssignedJob", ["worker", "started_at"])
+class Worker:
+    """Worker class.
+    The workers are sorted by release time. If the release time is the same for
+    both of them, workers are sorted by their thread_id.
+    """
+    
+    def __init__(self, thread_id, release_time=0):
+        self.thread_id = thread_id
+        self.release_time = release_time
+
+    def __lt__(self, other):
+        if self.release_time == other.release_time:
+            return self.thread_id < other.thread_id
+        return self.release_time < other.release_time
+
+    def __gt__(self, other):
+        if self.release_time == other.release_time:
+            return self.thread_id > other.thread_id
+        return self.release_time > other.release_time
 
 
-def assign_jobs(n_workers, jobs):
-    # TODO: replace this code with a faster algorithm.
-    result = []
-    next_free_time = [0] * n_workers
-    for job in jobs:
-        next_worker = min(range(n_workers), key=lambda w: next_free_time[w])
-        result.append(AssignedJob(next_worker, next_free_time[next_worker]))
-        next_free_time[next_worker] += job
+class JobQueue:
+    def read_data(self):
+        """Reads data from standard input."""
+        self.num_workers, m = map(int, input().split())
+        self.jobs = list(map(int, input().split()))
+        self.size = len(self.jobs)
+        assert m == self.size
 
-    return result
+    def write_response(self):
+        """Writes the response to standard output."""
+        for worker_id, start_time in self.result:
+            print(worker_id, start_time)
+
+    def assign_jobs(self):
+        """Assigns jobs to corresponding workers"""
+        self.result = []
+        self.worker_queue = [Worker(i) for i in range(self.num_workers)]
+
+        for job in self.jobs:
+            worker = heapq.heappop(self.worker_queue)
+
+            self.result.append((worker.thread_id, worker.release_time))
+
+            worker.release_time += job
+            heapq.heappush(self.worker_queue, worker)
+
+    def solve(self):
+        self.read_data()
+        self.assign_jobs()
+        self.write_response()
 
 
 def main():
-    n_workers, n_jobs = map(int, input().split())
-    jobs = list(map(int, input().split()))
-    assert len(jobs) == n_jobs
-
-    assigned_jobs = assign_jobs(n_workers, jobs)
-
-    for job in assigned_jobs:
-        print(job.worker, job.started_at)
+    job_queue = JobQueue()
+    job_queue.solve()
 
 
 if __name__ == "__main__":
--- Part II - Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
+++ Part II - Data Structures/week2_priority_queues_and_disjoint_sets/3_merging_tables/merging_tables.py
@@ -1,41 +1,79 @@
 # python3
 
-
-class Database:
-    def __init__(self, row_counts):
-        self.row_counts = row_counts
-        self.max_row_count = max(row_counts)
-        n_tables = len(row_counts)
-        self.ranks = [1] * n_tables
-        self.parents = list(range(n_tables))
-
-    def merge(self, src, dst):
-        src_parent = self.get_parent(src)
-        dst_parent = self.get_parent(dst)
-
-        if src_parent == dst_parent:
-            return False
-
-        # merge two components
-        # use union by rank heuristic
-        # update max_row_count with the new maximum table size
-        return True
-
-    def get_parent(self, table):
-        # find parent and compress path
-        return self.parents[table]
+import sys
 
 
-def main():
-    n_tables, n_queries = map(int, input().split())
-    counts = list(map(int, input().split()))
-    assert len(counts) == n_tables
-    db = Database(counts)
-    for i in range(n_queries):
-        dst, src = map(int, input().split())
-        db.merge(dst - 1, src - 1)
-        print(db.max_row_count)
+#class Database:
+#    def __init__(self, row_counts):
+#        self.row_counts = row_counts
+#        self.max_row_count = max(row_counts)
+#        n_tables = len(row_counts)
+#        self.ranks = [1] * n_tables
+#        self.parents = list(range(n_tables))
+#
+#    def merge(self, src, dst):
+#        src_parent = self.get_parent(src)
+#        dst_parent = self.get_parent(dst)
+#
+#        if src_parent == dst_parent:
+#            return False
+#
+#        # merge two components
+#        # use union by rank heuristic
+#        # update max_row_count with the new maximum table size
+#        return True
+#
+#    def get_parent(self, table):
+#        # find parent and compress path
+#        return self.parents[table]
 
 
-if __name__ == "__main__":
-    main()
+n, m = map(int, sys.stdin.readline().split())
+lines = list(map(int, sys.stdin.readline().split()))
+rank = [1] * n
+parent = list(range(0, n))
+ans = [max(lines)]
+act = {}
+
+def getParent(table):
+    if table != parent[table]:
+        parent[table] = getParent(parent[table])
+    return parent[table]
+
+
+def merge(destination, source):
+    realDestination, realSource = getParent(destination), getParent(source)
+    lineRoot = 0
+
+    if realDestination == realSource:
+        return False
+
+    if rank[realDestination] > rank[realSource]:
+        parent[realSource] = realDestination
+        lines[realDestination] += lines[realSource]
+        lineRoot = lines[realDestination]
+        lines[realSource] = 0
+
+    elif rank[realDestination] == rank[realSource]:
+        parent[realSource] = realDestination
+        lines[realDestination] += lines[realSource]
+        lineRoot = lines[realDestination]
+        lines[realSource] = 0
+        rank[realDestination] += 1
+
+    else:
+        parent[realDestination] = realSource
+        lines[realSource] += lines[realDestination]
+        lineRoot = lines[realSource]
+        lines[realDestination] = 0
+
+    if lineRoot > ans[0]:
+        ans[0] = lineRoot
+
+    return True
+
+
+for i in range(m):
+    destination, source = map(int, sys.stdin.readline().split())
+    merge(destination - 1, source - 1)
+    print(ans[0])
